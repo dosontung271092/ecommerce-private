c3d9875ecd668b60053d4ebe1d5fed8a093a355d
unknown <dosontung271092@gmail.com>
2022-12-30 19:10:47 +0700

    update code

--- assets/js/swiper-config.js
+++ assets/js/swiper-config.js
@@ -42,3 +42,36 @@ var swiper = new Swiper(".product__latest", {
         },
     }
 });
+
+// Post row
+var swiper = new Swiper(".post__latest", {
+    slidesPerView: 5,
+    spaceBetween: 5,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".post-row-header__btn--prev",
+        prevEl: ".post-row-header__btn--next",
+    },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        "@0.65": {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        "@1.65": {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+        "@2.00": {
+            slidesPerView: 5,
+            spaceBetween: 25,
+        },
+    }
+});
