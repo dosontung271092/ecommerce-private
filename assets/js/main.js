7a6a8849cc899d12a00695d928ade7827b0ba5f4
unknown <dosontung271092@gmail.com>
2023-01-03 12:02:58 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Display header search when user scroll top 200px
 window.onscroll = function (e) {
     if( window.scrollY >= 200 ){
         document.getElementById('header__scroll').style.display = "block";
